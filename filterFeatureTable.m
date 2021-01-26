c23c8b4d244d8c97d4b69a64705f9ebd571c7551
Serena Ding <sding@mrc-10942.lan>
2021-01-26 17:06:01 +0000

    made extracting Tierpsy_256 or Tierpsy_all features from featureTable more elegant

---
+++ filterFeatureTable.m
@@ -0,0 +1,38 @@
+%% Function filters features table by specified strain and feature requirements before classification tasks.
+
+%% Inputs:
+% featureTable: file-by-feature featureTable
+% classVar: String or cell array of strings containing the name of the variable to classify for. Each string must be a variable field of the featureTable.
+% strains: Cell array containing strains to yse for analysis.
+% featSetName: 'Tierpsy_256' or 'Tierpsy_all' 
+
+%% Outputs: 
+% featureTable: file-by-feature featureTable with non-feature metadata variables removed.
+% classLabels: file x 1 class labels to feed into the classification task. If multiple multiple variables are specied for classVar then classLabels is a struct where each field contains the labels for that variable.
+
+function [featureTable, classLabels,filenames] = filterFeatureTable(featureTable,classVar,strains,featSetName)
+
+%% process rows (observations) first
+
+% retain strains as specified
+strainLogInd = ismember(featureTable.strain_name,strains);
+featureTable = featureTable(strainLogInd,:);
+% get strain classification labels
+if ischar(classVar)
+    classLabels = featureTable.(classVar); 
+else
+    for varCtr = 1:numel(classVar)
+        var = classVar{varCtr};
+        classLabels.(var) = featureTable.(var);
+    end
+end
+
+%% process columns (features) second
+
+% load feature sets 
+load('featureSet/features.mat','features');
+featNames = features.(featSetName);
+% trim table down to specified feature sets
+featureTable = featureTable(:,featNames);
+
+end
